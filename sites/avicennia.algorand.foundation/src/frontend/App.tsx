6a82745a58babdd8becc76363cb8366289a36302
Michael J Feher <github@phearzero.com>
2024-01-17 08:19:16 -0600

    wip: add leaves

--- sites/avicennia.algorand.foundation/src/frontend/App.tsx
+++ sites/avicennia.algorand.foundation/src/frontend/App.tsx
@@ -37,39 +37,60 @@ export default function App() {
     }, [theme]);
 
     return (
-        <>
-        <AppBar position="sticky">
-            <Toolbar>
-                <IconButton
+      <>
+          <AppBar position="sticky">
+              <Toolbar>
+                  <IconButton
                     size="large"
                     edge="start"
                     color="inherit"
                     aria-label="menu"
                     disabled={state === 'start'}
-                >
-                    <Avatar alt="Remy Sharp" src="/logo.png" />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Avicennia
-                </Typography>
-                <IconButton onClick={()=>{setState(state === 'registered' ? 'connected' : 'start')}} aria-label="delete" disabled={state === 'start'} color="inherit">
-                    <NavigateBeforeIcon />
-                </IconButton>
-                <IconButton onClick={()=>{setState(state === 'start' ? 'connected' : 'registered')}} aria-label="delete" disabled={state === 'registered'} color="inherit">
-                    <NavigateNextIcon />
-                </IconButton>
-                <ConnectModal color="inherit"/>
-                <IconButton sx={{ ml: 1 }} onClick={colorMode.toggle} color="inherit">
-                    {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-                </IconButton>
-            </Toolbar>
-        </AppBar>
-        <Container component="main" maxWidth="md" sx={{display: "flex", height: "calc(100vh - 64px)"}}>
-            <Box sx={{ my: 4, flex: 1 }}>
-                <Content/>
-            </Box>
-        </Container>
+                  >
+                      <Avatar alt="Remy Sharp" src="/logo.png" />
+                  </IconButton>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                      Avicennia
+                  </Typography>
+                  <IconButton onClick={() => {
+                      setState(state === 'registered' ? 'connected' : 'start')
+                  }} aria-label="delete" disabled={state === 'start'} color="inherit">
+                      <NavigateBeforeIcon />
+                  </IconButton>
+                  <IconButton onClick={() => {
+                      setState(state === 'start' ? 'connected' : 'registered')
+                  }} aria-label="delete" disabled={state === 'registered'} color="inherit">
+                      <NavigateNextIcon />
+                  </IconButton>
+                  <ConnectModal color="inherit" />
+                  <IconButton sx={{ ml: 1 }} onClick={colorMode.toggle} color="inherit">
+                      {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                  </IconButton>
+              </Toolbar>
+          </AppBar>
+          <div id="leaves" style={{zIndex: -1}}>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+          </div>
+          <Container component="main" maxWidth="md" sx={{ display: "flex", height: "calc(100vh - 64px)", overflowY: "auto" }}>
+              <Box sx={{ my: 4, flex: 1 }}>
+                  <Content />
+              </Box>
+          </Container>
 
-        </>
+      </>
     );
 }
